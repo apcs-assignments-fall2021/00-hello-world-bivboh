fc800ff465d5c13b2aa6fc0d766ae927325e8d03
athenian <bivboh@gmail.com>
2021-09-01 10:15:22 -0700

    first

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,7 +1,7 @@
 public class MyMain {
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
+        System.out.println("Hello world!");
 
         // Write some code here!
 
